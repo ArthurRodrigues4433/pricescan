db949cacededd293173a2a6be85ecc7d70ee8085
arthur-rodrigues <arthurodrigues4433@gmail.com>
2026-03-03 10:25:40 -0300

    Adicionei validação no forms, para quantidade e preço  n ser >= 0

--- src/forms.py
+++ src/forms.py
@@ -6,3 +6,19 @@ class ItemCompraForm(forms.ModelForm):
     class Meta:
         model = ItemCompra
         fields = ["nome", "preco_unitario", "quantidade", "foto"]
+
+    def clean_preco_unitario(self):
+        preco = self.cleaned_data.get("preco_unitario")
+
+        if preco is not None and preco <= 0:
+            raise forms.ValidationError("O preço deve ser maior que zero.")
+
+        return preco
+
+    def clean_quantidade(self):
+        quantidade = self.cleaned_data.get("quantidade")
+
+        if quantidade is not None and quantidade <= 0:
+            raise forms.ValidationError("A quantidade deve ser maior que zero.")
+
+        return quantidade
